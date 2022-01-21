46a37ad9c4f24908803d435f5d1f90a7b5f162b7
plizaso <58313867+plizaso@users.noreply.github.com>
2022-01-21 19:34:09 -0300

    Implementación tarea 2
    Se implementó la estructura del html pedida en el enunciado y la funcionalidad del botón "Ingresar" en tarea2.js. Además se agregó un botón de reset.

--- tarea2/tarea-2.js
+++ tarea2/tarea-2.js
@@ -1,2 +1,24 @@
-console.log("Tarea 2");
-alert("Tarea 2");
+$botonIngresar = document.querySelector("#boton-ingresar");
+$botonIngresar.onclick = function () {
+    const primerNombreUsuario = document.querySelector("#primer-nombre").value.toUpperCase();
+    const segundoNombreUsuario = document.querySelector("#segundo-nombre").value.toUpperCase();
+    const apellidosUsuario = document.querySelector("#apellidos").value.toUpperCase();
+    const edadUsuario = Number(document.querySelector("#edad").value);
+    if (primerNombreUsuario === "" || apellidosUsuario === "" || edadUsuario === 0) {
+        alert("Completar todos los campos obligatorios.");
+    } else if (edadUsuario <= 0) {
+        alert("La edad debe ser mayor que 0");
+    } else {
+        document.querySelector("h1").textContent = `Bienvenido, ${primerNombreUsuario}!`;
+        const infoUsuario = `Nombre/s: ${primerNombreUsuario} ${segundoNombreUsuario} | Apellido/s: ${apellidosUsuario} | Edad: ${edadUsuario} años`;
+        document.querySelector("#resultado").textContent = infoUsuario;
+        //$botonIngresar.disabled = true;
+    }
+    //console.log("Boton Ingresar fue presionado.");
+    return false;
+}
+document.querySelector("#reset").onclick = function () {
+    document.querySelector("h1").textContent = "Bienvenido!";
+    document.querySelector("#resultado").textContent = "";
+    //$botonIngresar.disabled = false;
+}
