9d0b62aff42d4d9390564c839e0e86eb7a47cf2b
plizaso <58313867+plizaso@users.noreply.github.com>
2022-01-24 20:18:19 -0300

    Refactorización de código tarea3
    Estaba declarando varias veces la misma función(con distinto nombre) para horas, minutos y segundos.

--- tarea3/tarea-3.js
+++ tarea3/tarea-3.js
@@ -1,59 +1,31 @@
 //console.log("Tarea 3");
 //alert("Tarea 3");
 
-function obtenerHoras() {
-    const $horas = document.querySelectorAll("input.horas");
-    const horas = [];
-    for (let i = 0; i < $horas.length; i++) {
-        horas.push(Number($horas[i].value));
+function obtenerNumeros($inputs) {
+    const numeros = [];
+    for (let i = 0; i < $inputs.length; i++) {
+        numeros.push(Number($inputs[i].value));
     }
-    return horas;
+    return numeros;
 }
-function obtenerMinutos() {
-    const $minutos = document.querySelectorAll("input.minutos");
-    const minutos = [];
-    for (let i = 0; i < $minutos.length; i++) {
-        minutos.push(Number($minutos[i].value));
-    }
-    return minutos;
-}
-function obtenerSegundos() {
-    const $segundos = document.querySelectorAll("input.segundos");
-    const segundos = [];
-    for (let i = 0; i < $segundos.length; i++) {
-        segundos.push(Number($segundos[i].value));
-    }
-    return segundos;
-}
-function sumarHoras(horas) {
+
+function sumarNumeros(array) {
     let sumaTotal = 0;
-    for (let i = 0; i < horas.length; i++) {
-        sumaTotal = sumaTotal + horas[i];
-    }
-    return sumaTotal;
-}
-function sumarMinutos(minutos) {
-    let sumaTotal = 0;
-    for (let i = 0; i < minutos.length; i++) {
-        sumaTotal = sumaTotal + minutos[i];
-    }
-    return sumaTotal;
-}
-function sumarSegundos(segundos) {
-    let sumaTotal = 0;
-    for (let i = 0; i < segundos.length; i++) {
-        sumaTotal = sumaTotal + segundos[i];
+    for (let i = 0; i < array.length; i++) {
+        sumaTotal += array[i];
     }
     return sumaTotal;
 }
 
 document.querySelector("button").onclick = function () {
-    let horas = sumarHoras(obtenerHoras());
-    let minutos = sumarMinutos(obtenerMinutos());
-    let segundos = sumarSegundos(obtenerSegundos());
-    minutos = minutos + Math.trunc(segundos / 60);
-    segundos = segundos % 60;
-    horas = horas + Math.trunc(minutos / 60);
-    minutos = minutos % 60;
+    let horas = sumarNumeros(obtenerNumeros(document.querySelectorAll("input.horas")))
+    let minutos = sumarNumeros(obtenerNumeros(document.querySelectorAll("input.minutos")))
+    let segundos = sumarNumeros(obtenerNumeros(document.querySelectorAll("input.segundos")))
+    const minutosEnHora = 60;
+    const segundosEnMinuto = 60;
+    minutos += Math.trunc(segundos / segundosEnMinuto);
+    segundos = segundos % segundosEnMinuto;
+    horas += Math.trunc(minutos / minutosEnHora);
+    minutos = minutos % minutosEnHora;
     document.querySelector("strong").textContent = `Tiempo total: ${horas} Horas, ${minutos} Minutos, ${segundos} Segundos`
 }
